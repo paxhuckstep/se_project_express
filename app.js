959e7004ccdbecc814bf23a3e1fb4238d9a89b89
BigGarrison <paxhuckstep@gmail.com>
2025-03-03 13:26:29 -0700

    changed port to 3002

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 // const limiter = require("./middlewares/rateLimit");
 
 const app = express();
-const { PORT = 3001 } = process.env;
+const { PORT = 3002 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
